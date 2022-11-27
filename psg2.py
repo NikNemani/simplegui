8d232cb2ad7bda6384219195b7bc4c1b5760e414
nik nemani <niknemani@gmail.com>
2022-11-26 20:08:48 -0500

    psg2 updated with checkboxes for video and audio

--- psg2.py
+++ psg2.py
@@ -17,11 +17,15 @@ import yt_dlp
     Copyright 2020 PySimpleGUI.org
 """
 
-def my_hook(d):
+def audio_hook(d):
     #final_filename  = d.get('info_dict').get('filepath')
     #print(f'final_filename : {final_filename}')
-    window['-TOUT-'].update(d['status'])
-    #window['-TOUT-'].update(final_filename)
+    window['-TOUT1-'].update(d['status'])
+    #window['-TOUT1-'].update(final_filename)
+    window.refresh()
+
+def video_hook(d):
+    window['-TOUT2-'].update(d['status'])
     window.refresh()
 
 def download_audio(url, dlfn, folder) :
@@ -44,7 +48,7 @@ def download_audio(url, dlfn, folder) :
     ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': dlfilename,
-        'progress_hooks': [my_hook],
+        'progress_hooks': [audio_hook],
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
@@ -70,24 +74,69 @@ def download_audio(url, dlfn, folder) :
         #    ydl.download(urls)
     except Exception as e:
         rtnval = f'** Error {e} **'
-        print(type(rtnval))
     return rtnval
+
+def download_video(url, dlfn, folder) :
+    '''
+    Will download audio from url
+    :param url: url to download audio from
+    :type url:  string
+    :param dlfn: Filename to save the downloaded audio
+    :type dlfn:  string
+    :param folder: Folder for the downloaded audio file
+    :type folder:  string
+    :return: string
+    :rtype: string
+    '''
+    urls = []
+    urls.append(url)
+    dlfilename = folder + '/' + dlfn + '.%(ext)s'
+    #filename = os.path.normpath(folder + '/' + dlfn + '.%(ext)s')
+    print(dlfilename)
+    ydl_opts = {
+        'format': 'best[ext=mp4]',
+        'outtmpl': dlfilename,
+        'progress_hooks': [video_hook],
+    }
+    try :
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            #info = ydl.extract_info(url, download=True)
+            ydl.download(urls)
+            rtnval = "Video Downloaded"
+            #file_path = ydl.prepare_filename(info, outtmpl= dlfilename)
+
+        #print(type(info))
+        #print(file_path)
+        #details = ydl.sanitize_info(info)
+        #with open(os.path.normpath(folder + '/rkntest.txt'),'w') as data: 
+        #    data.write(str(details))
+        #print(type(details))
+        # ℹ️ ydl.sanitize_info makes the info json-serializable
+        #print(json.dumps(ydl.sanitize_info(info)))
+        #with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        #    ydl.download(urls)
+    except Exception as e:
+        rtnval = f'** Error {e} **'
+    return rtnval
+
 
 # --------------------------------- Define Layout ---------------------------------
 
 # First the window layout...2 columns
 final_filename  = ""
 
-left_col = [
+one_col = [
     [sg.Text('Url'), sg.In(size=(40,2), enable_events=True ,key='-URL-')],    
     [sg.Text('Download filename'), sg.In(size=(40,2), enable_events=True ,key='-DLFN-')],    
     [sg.Text('Download Folder'), sg.In(size=(40,2), enable_events=True ,key='-FOLDER-'), sg.FolderBrowse()],
-[sg.Text(size=(40,2), key='-TOUT-')],
-              [sg.Button(button_text = "Download", disabled = True,key='-Download-'), sg.Exit()]
+    [sg.Checkbox('Video', enable_events=True, key='-VIDEOCB-', disabled = False), sg.Checkbox('Audio', enable_events=True, key='-AUDIOCB-', disabled = False)],
+    [sg.Text(size=(40,2), key='-TOUT1-')],
+    [sg.Text(size=(40,2), key='-TOUT2-')],
+    [sg.Button(button_text = "Download", disabled = True,key='-Download-'), sg.Exit()]
 ]
 
 # ----- Full layout -----
-layout = [[sg.Column(left_col, element_justification='c')]]
+layout = [[sg.Column(one_col, element_justification='c')]]
 
 # --------------------------------- Create Window ---------------------------------
 window = sg.Window('Audio Downloader', layout,resizable=True)
@@ -98,34 +147,70 @@ folder = ""
 url = ""
 dlfn = ""
 
+audiocb = False
+videocb = False
+
 while True:
     event, values = window.read()
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
     if event == '-FOLDER-':  # Download Folder name entered
         folder = values['-FOLDER-']
-        window['-TOUT-'].update("")
-        if folder != "" and url != "" and dlfn != "":
+        window['-TOUT1-'].update("")
+        window['-TOUT2-'].update("")
+        if folder != "" and url != "" and dlfn != "" and (audiocb or videocb):
             window['-Download-'].update(disabled = False)
+        else :
+            window['-Download-'].update(disabled = True)
     elif event == '-URL-':    # url entered
         url = values['-URL-'] 
-        window['-TOUT-'].update("")
-        if folder != "" and url != "" and dlfn != "":
+        window['-TOUT1-'].update("")
+        window['-TOUT2-'].update("")
+        if folder != "" and url != "" and dlfn != "" and (audiocb or videocb):
             window['-Download-'].update(disabled = False)
+        else :
+            window['-Download-'].update(disabled = True)
     elif event == '-DLFN-':    # Download Filename entered
         dlfn = values['-DLFN-'] 
-        window['-TOUT-'].update("")
-        if folder != "" and url != "" and dlfn != "":
+        window['-TOUT1-'].update("")
+        window['-TOUT2-'].update("")
+        if folder != "" and url != "" and dlfn != "" and (audiocb or videocb):
             window['-Download-'].update(disabled = False)
+        else :
+            window['-Download-'].update(disabled = True)
+    elif event == '-AUDIOCB-':    # Audio Checkbox
+        window['-TOUT1-'].update("")
+        window['-TOUT2-'].update("")
+        audiocb = values['-AUDIOCB-'] 
+        if folder != "" and url != "" and dlfn != "" and (audiocb or videocb):
+            window['-Download-'].update(disabled = False)
+        else :
+            window['-Download-'].update(disabled = True)
+    elif event == '-VIDEOCB-':    # Audio Checkbox
+        window['-TOUT1-'].update("")
+        window['-TOUT2-'].update("")
+        videocb = values['-VIDEOCB-']  
+        if folder != "" and url != "" and dlfn != "" and (audiocb or videocb):
+            window['-Download-'].update(disabled = False)
+        else :
+            window['-Download-'].update(disabled = True)
     elif event == '-Download-': 
         try:
             window['-Download-'].update(disabled = True)
-            rtnval = download_audio(url,dlfn, folder)
-            window['-TOUT-'].update(rtnval)
+            if videocb :
+                rtnval = download_video(url,dlfn, folder)
+                window['-TOUT2-'].update(rtnval)
+            if audiocb :
+                rtnval = download_audio(url,dlfn, folder)
+                window['-TOUT1-'].update(rtnval)
             window['-URL-'].update('')
             window['-DLFN-'].update('')
+            window['-VIDEOCB-'].update(value =  False)
+            window['-AUDIOCB-'].update(value =  False)
             url = ""
             dlfn = ""
+            audiocb = False
+            videocb = False
             window['-URL-'].set_focus(force = True)
             window.refresh()
         except Exception as E:
